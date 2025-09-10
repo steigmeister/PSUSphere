91fca76936f1cb0fd7a66a2246881f0ce11c9755
steigmeister <steigmeister@gmail.com>
2025-09-10 09:11:37 +0800

    made some changes in urls.py

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -21,6 +21,6 @@ from studentorg import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('', views.HomePageView.as_view(), name='home')
+    path('', views.HomePageView.as_view(), name='home'),
     path('organization_list', OrganizationList.as_view(), name='organization-list'),
 ]
